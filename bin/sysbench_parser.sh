3629314f882bd99fbb564c179a716a3165ff4ec5
Dmitry Volkov <dmitry.volkov@mariadb.com>
2022-05-14 22:28:58 -0700

    + permissions for db

--- bin/sysbench_parser.sh
+++ bin/sysbench_parser.sh
@@ -5,5 +5,10 @@ WORKLOAD_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../ && pwd)
 PYTHONPATH=$WORKLOAD_DIR
 export PYTHONPATH
 
+SQLITE_DIR="/xbench/db"
+SQLITE_DB="metrics.db"
+
+sudo chmod -R 777 $SQLITE_DIR/$SQLITE_DB
+
 cd $WORKLOAD_DIR
 bin/sysbench_parser.py
